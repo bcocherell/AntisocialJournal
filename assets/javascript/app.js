20255371e68330b2591a2465a3121504a186d673
Jon <mockbee2000@gmail.com>
2017-12-02 13:55:18 -0500

    first firebase set up

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+//https://firebase.google.com/docs/auth/web/google-signin
+// firebase key for the project 
+
+<script src="https://www.gstatic.com/firebasejs/4.7.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD0ZB74rJGKM-S0jSnWJxGpheUsc2FEzf0",
+    authDomain: "antisocial-network-187818.firebaseapp.com",
+    databaseURL: "https://antisocial-network-187818.firebaseio.com",
+    projectId: "antisocial-network-187818",
+    storageBucket: "",
+    messagingSenderId: "538567016572"
+  };
+  firebase.initializeApp(config);
+</script>
