a4c6804f1d9e87029ef5bc264971242b1836b3d4
David Bauer <bauer.david@att.net>
2017-12-02 18:58:29 -0500

    added links and scripts for NYT API and DatePicker UI

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,7 @@
 //https://firebase.google.com/docs/auth/web/google-signin
 // firebase key for the project 
 
-<script src="https://www.gstatic.com/firebasejs/4.7.0/firebase.js"></script>
-<script>
+
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD0ZB74rJGKM-S0jSnWJxGpheUsc2FEzf0",
@@ -13,4 +12,26 @@
     messagingSenderId: "538567016572"
   };
   firebase.initializeApp(config);
-</script>
+
+  var userDate = "19720501";	//Year must be in the format YYYYMMDD
+
+  var urlNYT = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  urlNYT += '?' + $.param({
+		'api-key': "421ad4f235d44b1d98214fda597d20bd",	//NY Times API key
+		'begin_date': userDate,
+		'end_date': userDate,
+		//This will return just a snippet of the article, including the web_url will allow
+		//the user to investigate further
+		'fl': "web_url, snippet",	//could also use lead_paragraph instead of snippet
+		'page': 0,	//a value of 0 returns the first 10 items (0-9), 1 returns (10-19), etc.
+		// 'facet_field': "section_name",
+		// 'facet_filter': "true"
+	});
+	$.ajax({
+  	url: urlNYT,
+  	method: 'GET',
+	}).done(function(result) {
+  	console.log(result);
+	}).fail(function(err) {
+  	throw err;
+	});
